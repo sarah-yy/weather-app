97ba5e02bc983c9249cdd398c1fc1883488b5bdb
Thong Yuan Yu Sarah <thong.yuan.yu.sarah@gmail.com>
2025-06-09 17:40:11 +0800

    Add Inter font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-sky-500 text-center">
+      <h1 className="text-sky-500 text-center font-semibold">
         This is a beginning React project
       </h1>
     </>
