e20a5399b309bf5888577244b6bee734972eee4d
Thong Yuan Yu Sarah <thong.yuan.yu.sarah@gmail.com>
2025-06-09 17:11:39 +0800

    Setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import tsParser from "@typescript-eslint/parser";
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/types/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      parser: tsParser,
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      "@stylistic/ts": stylisticTs,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +25,34 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "@stylistic/ts/comma-dangle": [
+        "warn",
+        "always-multiline",
+      ],
+      "@stylistic/ts/block-spacing": "warn",
+      "@stylistic/ts/brace-style": "warn",
+      "@stylistic/ts/comma-spacing": ["warn", { "before": false, "after": true }],
+      "@stylistic/ts/quotes": ["warn", "double"],
+      "@stylistic/ts/semi": ["warn", "always", {
+        "omitLastInOneLineBlock": true,
+      }],
+      "@stylistic/ts/space-before-blocks": "warn",
+      "@stylistic/ts/space-before-function-paren": ["warn", {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "always",
+      }],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      camelcase: "off",
+      "default-case": "error",
+      "no-dupe-args": "error",
+      "no-unused-vars": "error",
+      "no-extra-boolean-cast": "off",
     },
   },
-)
+);
